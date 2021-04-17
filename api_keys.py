7d932f263b2a09fe354fd0b61fe111662824fb13
Xiaoran Li <74216423+xiaoran-w-li@users.noreply.github.com>
2021-04-16 21:53:28 -0400

    vacation.py updated

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "97cc18618ebf570bc3180b59f1302b4c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAsemiaEIgLLP88pXW5phycwRojx2llyzE"
